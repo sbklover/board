bb4b0918740fa6556fe8d5c25a4da0385188bff1
SBK <sbklover@gmail.com>
2026-08-04 17:24:42 +0900

    26.08.04 17:20 게시판 수정

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.board.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
@@ -11,8 +12,17 @@ public class BoardController {
         return "login";
     }
 
-//    @GetMapping("/board")
-//    public String listAll() {
-//        return "list";
-//    }
+    // 새 글쓰기 폼 화면
+    @GetMapping("/board/new")
+    public String newForm() {
+        return "board/new"; // templates/board/new.html
+    }
+
+    // 새 글쓰기 폼 제출 처리
+    @PostMapping("/board/new")
+    public String write(String title, String content) {
+        // Board 객체로 감싸서 저장
+        // boardService.write(title, content);  // 서비스에 맞게 구현
+        return "redirect:/board";
+    }
 }
